6887b3dce098b797f1c1fd692b751a9dcef6da82
Sandars007 <allmightysandars@gmail.com>
2021-10-18 11:35:03 +0530

    added media query at 720px display

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,21 @@ scrollUp.addEventListener("click",()=>{
         behavior:"smooth",
     });
 });
+
+//720px screen nav bar for hamburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click",()=>{
+    ul.classList.toggle("show");
+});
+
+const navlink = document.querySelectorAll(".nav-link");
+
+navlink.forEach((link) =>{
+    link.addEventListener('click',()=>{
+        ul.classList.remove("show");
+    });
+});
